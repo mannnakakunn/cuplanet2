2d98b8deb24ff67826702475425037a0447d5d4e
Mannnakakunn <mannnakakunn@gmail.com>
2015-10-06 17:53:07 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cuplanet2::Application.config.secret_key_base = '2433324ac7d56ede8d6cb44fcdbb0ee3f26fc2b59d49cd74ebe74a80de58947470b63e80509668196b05e23ce1d212686a130a64e494f2d165f7bdf4c16a8642'
